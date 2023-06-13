a3f31f32eba3ab407b52aa31e1030f52f5f368ef
Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>
2023-06-13 14:41:29 +0700

    Fix error get masyarakat data on profile index
    Signed-off-by: Ilham Muttaqien <ilham.muttaqien@students.amikom.ac.id>

--- application/controllers/User/ProfileController.php
+++ application/controllers/User/ProfileController.php
@@ -1,7 +1,8 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class ProfileController extends CI_Controller {
+class ProfileController extends CI_Controller
+{
 
 	public function __construct()
 	{
@@ -13,10 +14,14 @@ class ProfileController extends CI_Controller {
 	// List all your items
 	public function index()
 	{
-        $data['title'] = 'Profile';
+		$data['title'] = 'Profile';
 
-        $masyarakat = $this->db->get_where('masyarakat',['username' => $this->session->userdata('username')])->row_array();
-		$petugas = $this->db->get_where('petugas',['username' => $this->session->userdata('username')])->row_array();
+		$masyarakat = $this->db->get_where('masyarakat', ['username' => $this->session->userdata('username')])->row_array();
+		$detail_masyarakat = $this->db->get_where('masyarakat_detail', ['id_masyarakat' => $masyarakat['nik']])->row_array();
+
+		$masyarakat = array_merge($masyarakat, $detail_masyarakat);
+
+		$petugas = $this->db->get_where('petugas', ['username' => $this->session->userdata('username')])->row_array();
 
 		if ($masyarakat == TRUE) :
 			$data['user'] = $masyarakat;
@@ -24,12 +29,12 @@ class ProfileController extends CI_Controller {
 			$data['user'] = $petugas;
 		endif;
 
-        $this->load->view('_part/backend_head', $data);
-        $this->load->view('_part/backend_sidebar_v');
-        $this->load->view('_part/backend_topbar_v');
-        $this->load->view('user/profile');
-        $this->load->view('_part/backend_footer_v');
-        $this->load->view('_part/backend_foot');
+		$this->load->view('_part/backend_head', $data);
+		$this->load->view('_part/backend_sidebar_v');
+		$this->load->view('_part/backend_topbar_v');
+		$this->load->view('user/profile');
+		$this->load->view('_part/backend_footer_v');
+		$this->load->view('_part/backend_foot');
 	}
 }
 
