8c3bf08fa43b314c54baad648e65c36db7f06db1
Estu Fardani <andro.medh4@gmail.com>
2023-07-18 12:09:12 +0700

    fix tanggapan

--- database/pengaduan_masyarakat_ci.sql
+++ database/pengaduan_masyarakat_ci.sql
@@ -188,7 +188,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `tanggapan`;
 CREATE TABLE `tanggapan` (
-  `id_tanggapan` int(11) NOT NULL,
+  `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT,
   `id_pengaduan` bigint(16) NOT NULL,
   `tgl_tanggapan` date NOT NULL,
   `tanggapan` text NOT NULL,
@@ -196,7 +196,7 @@ CREATE TABLE `tanggapan` (
   PRIMARY KEY (`id_tanggapan`),
   KEY `id_pengaduan` (`id_pengaduan`),
   KEY `id_petugaskab` (`id_petugaskab`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tanggapan`
@@ -204,9 +204,7 @@ CREATE TABLE `tanggapan` (
 
 LOCK TABLES `tanggapan` WRITE;
 INSERT INTO `tanggapan` (`id_tanggapan`, `id_pengaduan`, `tgl_tanggapan`, `tanggapan`, `id_petugaskab`) VALUES
-(20,15,'2023-02-14','sedang didalami',6),
-(21,12,'2023-02-14','Sedang dalam proses',6),
-(25,27,'2023-06-19','Deskripsi kurang lengkap',2);
+(1,1,'2023-07-18','sedang didalami',2);
 UNLOCK TABLES;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
